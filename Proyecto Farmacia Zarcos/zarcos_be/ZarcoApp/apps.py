eeefaea72dfa053e48e6d65307ed22a511159af7
Charlyboy1993 <cjaramilloportilla@gmail.com>
2021-10-13 11:17:48 -0500

    Se actualiza el repo

--- Proyecto Farmacia Zarcos/zarcos_be/ZarcoApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ZarcoappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ZarcoApp'
--- Proyecto Farmacia Zarcos/zarcos_be/ZarcoApp/models/categoria.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Categoria(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    nombreCategoria = models.CharField('Categoria', max_length=100)
